e88eaff14eeb3f335db1ad6e516763f9b29d4e05
coryortega <coryortega3@gmail.com>
2020-03-07 14:49:34 -0800

    went over operators and if, elif, else statement(s)

---
+++ lessons/if_elif.py
@@ -0,0 +1,18 @@
+# OPERATORSSSSSSS
+# <,>,==,!=
+
+# age  = int(input('Enter your age:'))
+
+# if age < 10:
+#     #code block RIGHT HEREEEE
+#     print('you are young dawg')
+# elif age < 40:
+#     print('youre still gucci')
+# else: print('you are so old')
+
+meaty = input('Do you eat meat? (y/n):')
+
+if meaty != 'n':
+    print('here is the meaty menu...')
+else:
+    print('here is the veggie menu...')
