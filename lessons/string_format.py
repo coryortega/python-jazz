2ca2ef4d4c7564c7afb2e0d0a82fcce4094b57d2
coryortega <coryortega3@gmail.com>
2020-03-07 14:39:20 -0800

    learned string formatting in the forms of Format and f-strings

---
+++ lessons/string_format.py
@@ -0,0 +1,11 @@
+num1 = 3.1425473
+num2 = 10.29003952
+
+# print('num1 is', num1, 'and num2 is', num2)
+
+# FORMAT METHODDDDDDDDD
+# print('num1 is {0:.3} and num2 is {1:.3f}'.format(num1, num2))
+
+# USING F-STRINGS
+
+print(f'num 1 is {num1: .4f} and num 2 is {num2}')
