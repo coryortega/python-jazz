ae3af2f016311d93b307690762769bcbafc7df20
coryortega <coryortega3@gmail.com>
2020-03-07 15:05:03 -0800

    went over while loops, looked at the continue keyword

--- lessons/loops.py
+++ lessons/loops.py
@@ -1,15 +1,12 @@
-ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+age = 25
+num = 0
 
-# for ninja in ninjas:
-#     print(ninja)
-
-# for ninja in ninjas[1:3]:
-#     print(ninja)
-
-for ninja in ninjas:
-    if ninja == 'yoshi':
-        print(f'{ninja} - black belt')
-        # breaks out of the loop so ken is never reached
+while num < age:
+    if num == 0:
+        num += 1
+        continue
+    if num % 2 == 0:
+        print(num)
+    if num > 10:
         break
-    else:
-        print(ninja)
+    num +=1
